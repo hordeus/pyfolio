0a41d7a831e6ca1ab4493bacd28916b491cf2a38
Pedro <pedroadamas@gmail.com>
2020-05-02 15:00:42 +0200

    added the function to plot what I needed!!!

--- pyfolio/plotting.py
+++ pyfolio/plotting.py
@@ -26,6 +26,7 @@ import numpy as np
 import pandas as pd
 import pytz
 import scipy as sp
+import six
 from matplotlib import figure
 from matplotlib.backends.backend_agg import FigureCanvasAgg
 from matplotlib.ticker import FuncFormatter
@@ -53,6 +54,33 @@ def customize(func):
         else:
             return func(*args, **kwargs)
     return call_w_context
+
+
+def render_mpl_table(data, col_width=3.0, row_height=0.625, font_size=14,
+                     header_color='#40466e', row_colors=['#f1f1f2', 'w'], edge_color='w',
+                     bbox=[0, 0, 1, 1], header_columns=0,
+                     ax=None, **kwargs):
+
+    data = data.reset_index()#.round(2)
+    # print(data)
+    if ax is None:
+        size = (np.array(data.shape[::-1]) + np.array([0, 1])) * np.array([col_width, row_height])
+        fig, ax = plt.subplots(figsize=size)
+        ax.axis('off')
+
+    mpl_table = ax.table(cellText=data.values, bbox=bbox, colLabels=data.columns, **kwargs)
+
+    mpl_table.auto_set_font_size(False)
+    mpl_table.set_fontsize(font_size)
+
+    for k, cell in six.iteritems(mpl_table._cells):
+        cell.set_edgecolor(edge_color)
+        if k[0] == 0 or k[1] < header_columns:
+            cell.set_text_props(weight='bold', color='w')
+            cell.set_facecolor(header_color)
+        else:
+            cell.set_facecolor(row_colors[k[0]%len(row_colors) ])
+    return ax
 
 
 def plotting_context(context='notebook', font_scale=1.5, rc=None):
@@ -649,6 +677,9 @@ def show_perf_stats(returns, factor_returns=None, positions=None,
             if stat in STAT_FUNCS_PCT:
                 perf_stats.loc[stat, column] = str(np.round(value * 100,
                                                             3)) + '%'
+            else:
+                perf_stats.loc[stat, column] = str(np.round(value,2))
+
     if header_rows is None:
         header_rows = date_rows
     else:
@@ -658,8 +689,10 @@ def show_perf_stats(returns, factor_returns=None, positions=None,
     utils.print_table(
         perf_stats,
         float_format='{0:.2f}'.format,
-        header_rows=header_rows,
+        header_rows=header_rows
     )
+
+    render_mpl_table(perf_stats, header_columns=0, col_width=3.0)
 
 
 def plot_returns(returns,
@@ -1674,6 +1707,11 @@ def show_worst_drawdown_periods(returns, top=5):
         name='Worst drawdown periods',
         float_format='{0:.2f}'.format,
     )
+
+    render_mpl_table(drawdown_df.sort_values('Net drawdown in %', ascending=False),
+        header_columns=0,
+        col_width=3.0)
+
 
 
 def plot_monthly_returns_timeseries(returns, ax=None, **kwargs):
