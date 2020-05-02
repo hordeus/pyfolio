5ab00b0043f01b38e0d1997235bb1ec774aa2dde
Pedro <pedroadamas@gmail.com>
2020-05-02 17:41:20 +0200

    moved renderer to the print table function

--- pyfolio/plotting.py
+++ pyfolio/plotting.py
@@ -26,7 +26,6 @@ import numpy as np
 import pandas as pd
 import pytz
 import scipy as sp
-import six
 from matplotlib import figure
 from matplotlib.backends.backend_agg import FigureCanvasAgg
 from matplotlib.ticker import FuncFormatter
@@ -54,33 +53,6 @@ def customize(func):
         else:
             return func(*args, **kwargs)
     return call_w_context
-
-
-def render_mpl_table(data, col_width=3.0, row_height=0.625, font_size=14,
-                     header_color='#40466e', row_colors=['#f1f1f2', 'w'], edge_color='w',
-                     bbox=[0, 0, 1, 1], header_columns=0,
-                     ax=None, **kwargs):
-
-    data = data.reset_index()#.round(2)
-    # print(data)
-    if ax is None:
-        size = (np.array(data.shape[::-1]) + np.array([0, 1])) * np.array([col_width, row_height])
-        fig, ax = plt.subplots(figsize=size)
-        ax.axis('off')
-
-    mpl_table = ax.table(cellText=data.values, bbox=bbox, colLabels=data.columns, **kwargs)
-
-    mpl_table.auto_set_font_size(False)
-    mpl_table.set_fontsize(font_size)
-
-    for k, cell in six.iteritems(mpl_table._cells):
-        cell.set_edgecolor(edge_color)
-        if k[0] == 0 or k[1] < header_columns:
-            cell.set_text_props(weight='bold', color='w')
-            cell.set_facecolor(header_color)
-        else:
-            cell.set_facecolor(row_colors[k[0]%len(row_colors) ])
-    return ax
 
 
 def plotting_context(context='notebook', font_scale=1.5, rc=None):
@@ -691,8 +663,6 @@ def show_perf_stats(returns, factor_returns=None, positions=None,
         float_format='{0:.2f}'.format,
         header_rows=header_rows
     )
-
-    render_mpl_table(perf_stats, header_columns=0, col_width=3.0)
 
 
 def plot_returns(returns,
@@ -1707,12 +1677,6 @@ def show_worst_drawdown_periods(returns, top=5):
         name='Worst drawdown periods',
         float_format='{0:.2f}'.format,
     )
-
-    render_mpl_table(drawdown_df.sort_values('Net drawdown in %', ascending=False),
-        header_columns=0,
-        col_width=3.0)
-
-
 
 def plot_monthly_returns_timeseries(returns, ax=None, **kwargs):
     """
--- pyfolio/utils.py
+++ pyfolio/utils.py
@@ -21,6 +21,8 @@ from itertools import cycle
 from matplotlib.pyplot import cm
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import six
 from IPython.display import display, HTML
 
 import empyrical.utils
@@ -111,6 +113,33 @@ def vectorize(func):
     return wrapper
 
 
+def render_mpl_table(data, col_width=3.0, row_height=0.625, font_size=14,
+                     header_color='#40466e', row_colors=['#f1f1f2', 'w'], edge_color='w',
+                     bbox=[0, 0, 1, 1], header_columns=0,
+                     ax=None, **kwargs):
+
+    data = data.reset_index()
+    if ax is None:
+        size = (np.array(data.shape[::-1]) + np.array([0, 1])) * np.array([col_width, row_height])
+        fig, ax = plt.subplots(figsize=size)
+        ax.axis('off')
+
+    mpl_table = ax.table(cellText=data.values, bbox=bbox, colLabels=data.columns, **kwargs)
+
+    mpl_table.auto_set_font_size(False)
+    mpl_table.set_fontsize(font_size)
+
+    for k, cell in six.iteritems(mpl_table._cells):
+        cell.set_edgecolor(edge_color)
+        if k[0] == 0 or k[1] < header_columns:
+            cell.set_text_props(weight='bold', color='w')
+            cell.set_facecolor(header_color)
+        else:
+            cell.set_facecolor(row_colors[k[0]%len(row_colors) ])
+    return ax
+
+
+
 def extract_rets_pos_txn_from_zipline(backtest):
     """
     Extract returns, positions, transactions and leverage from the
@@ -171,7 +200,8 @@ def print_table(table,
                 name=None,
                 float_format=None,
                 formatters=None,
-                header_rows=None):
+                header_rows=None,
+                show_in_python=True):
     """
     Pretty print a pandas DataFrame.
 
@@ -200,6 +230,12 @@ def print_table(table,
 
     if name is not None:
         table.columns.name = name
+
+    if show_in_python:
+        try:
+            render_mpl_table(table, header_columns=0, col_width=3.0)
+        except IndexError:
+            pass
 
     html = table.to_html(float_format=float_format, formatters=formatters)
 
